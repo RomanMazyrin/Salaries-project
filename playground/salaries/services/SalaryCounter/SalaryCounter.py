55353fe86b754d06cca8fc98ee33fbcb40445f5b
Roman Mazyrin <roma212008@yandex.ru>
2021-02-07 02:25:20 +0300

    add some metrics in salary counter and salary result tmpl

--- playground/salaries/services/SalaryCounter/SalaryCounter.py
+++ playground/salaries/services/SalaryCounter/SalaryCounter.py
@@ -1,3 +1,6 @@
+import json 
+import requests
+
 class SalaryCounter:
 
     def __init__(self, onpbx_client=None):
@@ -22,9 +25,36 @@ class SalaryCounter:
 
         sal = int((len(res_outbound) + len(res_inbound)) * employee.one_call_cost)
 
+        request_params = {
+            'auth_key': 'oweiurghw85gh74o8m7h48',
+            "filter": json.dumps({
+                "closed_at": {
+                    "from": timestamp_from,
+                    "to": timestamp_to
+                },
+                "responsible_user_id": employee.amocrm_id,
+                "statuses": [
+                    {"status_id": 142, "pipeline_id": 1212574},
+                    {"status_id": 142, "pipeline_id": 1693621},
+                    {"status_id": 142, "pipeline_id": 1693720},
+                    {"status_id": 142, "pipeline_id": 3941655},
+                ]
+            })
+        }
+
+        res = requests.get("http://localhost:9898/deltasales/get-leads-by-filter", params=request_params)
+
         return {
             "money": sal,
             'calls_outbound_count': len(res_outbound),
             'calls_inbound_count': len(res_inbound),
             'calls_total_count': len(res_inbound) + len(res_outbound),
+            'licenses_amount': len([lead for lead in res.json()['leads'] if lead['pipeline_id'] == 1212574]),
+            'licenses_sum': sum([lead['price'] for lead in res.json()['leads'] if lead['pipeline_id'] == 1212574]),
+            'widgets_amount': len([lead for lead in res.json()['leads'] if lead['pipeline_id'] == 1693720]),
+            'widgets_sum': sum([lead['price'] for lead in res.json()['leads'] if lead['pipeline_id'] == 1693720]),
+            'projects_amount': len([lead for lead in res.json()['leads'] if lead['pipeline_id'] == 1693621]),
+            'projects_sum': sum([lead['price'] for lead in res.json()['leads'] if lead['pipeline_id'] == 1693621]),
+            'courses_amount': len([lead for lead in res.json()['leads'] if lead['pipeline_id'] == 3941655]),
+            'courses_sum': sum([lead['price'] for lead in res.json()['leads'] if lead['pipeline_id'] == 3941655])
         }
