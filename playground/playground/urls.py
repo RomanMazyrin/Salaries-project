ef026385efad9a50cd4e73dc7ba93beb1d0d2ca2
Roman Mazyrin <roma212008@yandex.ru>
2021-02-25 12:35:26 +0300

    some design and url path for empty URI

--- playground/playground/urls.py
+++ playground/playground/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('', include(('salaries.urls'))),
     path('admin/', admin.site.urls),
     path('salaries/', include('salaries.urls'))
 ]
