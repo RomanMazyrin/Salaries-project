e364a393c8d93173cabf8f8ac9a66ca5f9f3c52d
Roman Mazyrin <roma212008@yandex.ru>
2021-02-27 01:02:22 +0300

    restart policy and default values to web service

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+TARGET_PORT=${PORT:-8000}
+TARGET_NUM_OF_WORKERS=${NUM_OF_WORKERS:-2}
+
 pipenv run python3 manage.py collectstatic --no-input
 pipenv run python3 manage.py migrate --no-input
 
-pipenv run gunicorn -w=$NUM_OF_WORKERS playground.wsgi --bind 0.0.0.0:$PORT
+pipenv run gunicorn -w=$TARGET_NUM_OF_WORKERS playground.wsgi --bind 0.0.0.0:$TARGET_PORT
