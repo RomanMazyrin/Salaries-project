ae5b0d15451fee440086682ac0cdb49d0a5fffe8
Roman Mazyrin <roma212008@yandex.ru>
2022-07-17 16:30:50 +0300

    fix employeesGroup

--- src/salaries/models/EmployeesGroup.py
+++ src/salaries/models/EmployeesGroup.py
@@ -7,9 +7,9 @@ class EmployeeGroup(models.Model):
 
     group_name = models.CharField('Название группы', max_length=250)
     employee_list = models.ManyToManyField(
-        Employee, related_name='groups_as_employee', verbose_name='Список участников')
+        Employee, related_name='groups_as_employee', verbose_name='Список участников', blank=True)
     group_heads = models.ManyToManyField(
-        Employee, related_name='groups_as_head', verbose_name='Список руководителей')
+        Employee, related_name='groups_as_head', verbose_name='Список руководителей', blank=True)
 
     def __str__(self):
         return self.group_name
